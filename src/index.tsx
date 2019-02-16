d0021fcf0951a27a236bf7bc9c7e69b4eceaeece
salih kemaloğlu <salihkem@outlook.com>
2019-02-16 16:24:55 +0300

    fix roter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AppRouter } from './router';
+import { AppRouter } from './components/router';
 
 ReactDOM.render(
   <AppRouter />
